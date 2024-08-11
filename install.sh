8d28d744dc1353af89689f0746d42910b4811ee3
Valeriy <98162870+abc-valera@users.noreply.github.com>
2024-08-11 11:18:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -51,6 +51,7 @@ sudo dnf install gnome-tweak-tool -y
 sudo dnf install vim -y
 sudo dnf install golang -y
 sudo dnf install nodejs -y
+sudo dnf install helm -y
 
 sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
 sudo dnf install terraform -y
@@ -58,7 +59,7 @@ sudo dnf install terraform -y
 # Step 2.2 Build applications
 go install github.com/go-task/task/v3/cmd/task@latest
 
-# Step 2.2 Install applications with flatpak
+# Step 2.3 Install applications with flatpak
 flatpak install flathub org.onlyoffice.desktopeditors -y
 flatpak install flathub org.videolan.VLC -y
 flatpak install flathub org.mozilla.firefox -y
@@ -66,13 +67,13 @@ flatpak install flathub org.gimp.GIMP -y
 flatpak install flathub org.telegram.desktop -y
 flatpak install flathub com.spotify.Client -y
 
-# Step 2.3 Install VSCode
+# Step 2.4 Install VSCode
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo >/dev/null
 
 sudo dnf install code -y
 
-# Step 2.4 Install Docker
+# Step 2.5 Install Docker
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo -y
 sudo dnf install docker-ce docker-ce-cli containerd.io -y
 sudo systemctl enable docker
